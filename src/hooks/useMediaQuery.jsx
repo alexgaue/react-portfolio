7c7dc77415cb7dd45f9e02c54d3460ea5f50d543
Alexander Aue <90017324+alexgaue@users.noreply.github.com>
2025-02-02 16:56:50 -0500

    Init project using create-react-app

---
+++ src/hooks/useMediaQuery.jsx
@@ -0,0 +1,21 @@
+// determines if you match a breakpoint of a media screen size
+import { useState, useEffect } from "react";
+
+// pass in a query, and it tells us when it matches a particular breakpoint for screen size
+const useMediaQuery = (query) => {
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+        const media = window.matchMedia(query);
+        if (media.matches !== matches) {
+            setMatches(media.matches);
+        }
+        const listener = () => setMatches(media.matches);
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    }, [matches, query]);
+
+    return matches;
+}
+
+export default useMediaQuery;
