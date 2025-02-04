fd1150c83feacf6f427147b381bf4651c5a3fc01
Alexander Aue <90017324+alexgaue@users.noreply.github.com>
2025-02-04 00:21:39 -0500

    Add navbar and dotgroup for navigation

--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -2,20 +2,20 @@ import { useState, useEffect } from "react";
 
 // a custom hook that determines if the screen matches a given media query
 const useMediaQuery = (query) => {
-    const [matches, setMatches] = useState(false); // state that stores whether query matches screen size
+  const [matches, setMatches] = useState(false); // state that stores whether query matches screen size
 
-    // runs when matches or query changes
-    useEffect(() => {
-        const media = window.matchMedia(query); // query listener
-        if (media.matches !== matches) {
-            setMatches(media.matches);
-        }
-        const listener = () => setMatches(media.matches);
-        window.addEventListener("resize", listener); // add listener to window's resize event
-        return () => window.removeEventListener("resize", listener); //cleanup
-    }, [matches, query]);
+  // runs when matches or query changes
+  useEffect(() => {
+    const media = window.matchMedia(query); // query listener
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener); // add listener to window's resize event
+    return () => window.removeEventListener("resize", listener); //cleanup
+  }, [matches, query]);
 
-    return matches;
-}
+  return matches;
+};
 
 export default useMediaQuery;
